774e438d99e76883a1d7fbb930130b6772607005
SheerinShaikh2003 <shaikhsheerin77@gmail.com>
2023-09-07 15:52:33 +0530

    commit:3

--- food/views.py
+++ food/views.py
@@ -1,3 +1,23 @@
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from food.models import Item
 
 # Create your views here.
+
+def index(request):
+    itemlist = Item.objects.all()
+    
+    context = {
+        'itemlist':itemlist
+    }
+    return render(request,'food/index.html', context)
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    
+    context = {
+        'item':item
+    }
+    
+    return render(request, 'food/detail.html', context)
