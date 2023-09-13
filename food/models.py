fa0667e5c3de9a4652a701f7e22d63d1ccf3c343
SheerinShaikh2003 <shaikhsheerin77@gmail.com>
2023-09-13 16:39:23 +0530

    commit:15

--- food/models.py
+++ food/models.py
@@ -4,8 +4,16 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
+    prod_code =models.IntegerField(default=100)
+    for_user = models.CharField(
+          max_length=100,
+          default='xyz'
+        )
     item_name = models.CharField(max_length=50)
-    item_desc = models.CharField(max_length=300)
+    item_desc = models.CharField(
+        max_length=500,
+        default="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, reiciendis saepe accusantium fugiat illo reprehenderit quidoloribus molestiae recusandae? Recusandae ducimus accusamus et. Soluta asperiores rerum laboriosam ratione, rem sed."
+        )
     item_price = models.IntegerField()
     item_image = models.CharField(
       max_length=500,
