9b3697a2bfb3d8088e2610087410000b593dcd12
SheerinShaikh2003 <shaikhsheerin77@gmail.com>
2023-09-13 15:34:01 +0530

    commit:14

--- food/models.py
+++ food/models.py
@@ -8,7 +8,7 @@ class Item(models.Model):
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
     item_image = models.CharField(
-        max_length=500,
+      max_length=500,
       default="https://cdn-icons-png.flaticon.com/512/1377/1377194.png")
     
     def _str_(self):
