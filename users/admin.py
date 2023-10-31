3d89757ec898f06acd9264ec7689843b5ff61012
SheerinShaikh2003 <shaikhsheerin77@gmail.com>
2023-10-31 16:14:48 +0530

    commit:40

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from users.models import Profile, CusOrders
+from users.models import Profile, CusOrders , CusRatingFeedback
 admin.site.register(Profile)
 admin.site.register(CusOrders)
+admin.site.register(CusRatingFeedback)
 # Register your models here.
